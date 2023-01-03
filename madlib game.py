fb4c54f58b28605098030788a70c8b69e92267f2
ranyae22 <114307467+ranyae22@users.noreply.github.com>
2023-01-03 13:00:23 -0600

    Add files via upload

---
+++ madlib game.py
@@ -0,0 +1,8 @@
+Color = input("Pick a color: ")
+Number = input("Pick a number: ")
+Animal = input("Pick an animal: ")
+Job = input("Pick a job: ")
+Name = input("Pick a name: ")
+place = input("Pick a place: ")
+activity = input("Pick an activity: ")
+print("The", Color, Number, "year old", Animal, "was a", Job, "with", Name, "at", place, "and liked", activity, ".")
